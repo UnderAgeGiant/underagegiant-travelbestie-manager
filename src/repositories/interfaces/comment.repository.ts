79dc0d756a6f5bd2736d7cafd208c658d7b5ebec
Matias Fuentes Perez <pay_90@hotmail.com>
2026-06-10 14:38:54 -0400

    feat(comments): add findByAttractions to ICommentRepository interface

--- src/repositories/interfaces/comment.repository.ts
+++ src/repositories/interfaces/comment.repository.ts
@@ -3,4 +3,5 @@ import { Comment } from '../../types';
 export interface ICommentRepository {
   add(data: Omit<Comment, 'id' | 'createdAt'> & { userId: string }): Promise<Comment>;
   findByAttraction(attractionId: string): Promise<Comment[]>;
+  findByAttractions(ids: string[]): Promise<Record<string, Comment[]>>;
 }
